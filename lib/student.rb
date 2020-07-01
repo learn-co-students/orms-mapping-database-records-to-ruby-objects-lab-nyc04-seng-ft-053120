d308bd9cdcc9c2f553aafe7a13bfa523d64ea89b
kceron <karemiceron@gmail.com>
2020-07-01 08:30:23 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,15 @@ class Student
 
   #This method should return an array of all the students in grade 9.
   def self.all_students_in_grade_9
-
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE id = 1
+      SQL
+   
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
   end
 
   #This method should return an array of all the students below 12th grade.
